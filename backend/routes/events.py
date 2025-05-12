2612f763de1f116790a2f46308c288d55e54cbeb
VonkyKI <vastudymail@gmail.com>
2025-05-12 07:59:53 +0300

    1

--- backend/routes/events.py
+++ backend/routes/events.py
@@ -9,5 +9,25 @@ router = APIRouter()
 
 
 @router.get("/", response_model=List[EventSchema])
-def get_opinions(db: Session = Depends(get_db)):
-    return db.query(dimEvents).all()
+def get_opinions(db: Session = Depends(get_db),
+    relevance_score: Optional[int] = Query(None, description="Filter by relevancy score"),
+    event_hotness: Optional[int] = Query(None, description="Filter by event hotness"),
+    is_selected: Optional[int] = Query(None, description="Filter by is_selected"),
+):
+    query = db.query(dimEvents)
+
+    
+    if event_hotness is not None:
+        # Припускаємо, що в таблиці DimOpinion є поле, яке містить person_id
+        query = query.filter(dimEvents.event_hotness >= event_hotness)
+
+    if relevance_score is not None:
+        # Припускаємо, що в таблиці DimOpinion є поле, яке містить person_id
+        query = query.filter(dimEvents.relevance_score >= relevance_score)
+
+    if is_selected is not None:
+        # Припускаємо, що в таблиці DimOpinion є поле, яке містить person_id
+        query = query.filter(dimEvents.is_selected == is_selected)
+
+
+    return query().all()
